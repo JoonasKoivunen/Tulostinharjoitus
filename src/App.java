f354a5048e50278a2893bfe05b9f0bc434e8512d
Joonas Oskari Koivunen <koivunen.joonas04@gmail.com>
2025-10-19 19:52:01 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hei olen tulostinohjelma");
+
+        // Ohjelmantekijä Joonas
+
+        System.out.println("Ohjelman tekijä: Joonas");
+
+
+        // Luku1 muuttujan arvo on 5.0
+
+        System.out.println("luku1-muuttujan arvo on 5.0");
+
+        // Luku2 muuttujan arvo on 2.0
+
+        System.out.println("luku2-muuttujan arvo on 2.0");
+
+
+        // 5 * 2 = 10
+
+        double luku1 = 5;
+        double luku2 = 2;
+        double tulo = luku1 * luku2;
+
+
+        System.out.println(luku1 + " * " + luku2 + " = " + tulo);
+
+    }
+}
